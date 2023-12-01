23d6fb649497564b54350d653d11c4ec9305fec7
Christopher Kochera <ckochera@gmail.com>
2023-12-01 15:17:17 -0500

    Update JS for farenheit
    Added conditional to convert celcius to farenheit.

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const tempInput = document.getElementById('tempInput');
-const tempScaleList = document.getElementById('temperatures');
+const tempScaleList = document.getElementById('tempScaleList');
 const convertBtn = document.getElementById('convertBtn');
 const result = document.getElementById('result');
 
-
-function convertTemp(){
+function convertTemp() {
     let temperature = tempInput.value;
-    let convertedTemp = (temperature * (9/5)) + 32;
+
+    if (tempScaleList.value === "celcius") {
+        let celciusConvertedTemp = (temperature * (9/5)) + 32;
 
     const p = document.createElement('p');
-    p.textContent = temperature + " degrees Celcius is " + convertedTemp + " degrees Farenheit."
+    p.textContent = temperature + " degrees Celcius is " + celciusConvertedTemp + " degrees Farenheit."
     result.appendChild(p);
-}
+    } else {
+        let farenConvertedTemp = (temperature - 32) * 5/9;
 
+        const p = document.createElement('p');
+        p.textContent = temperature + " degrees Farenheit is " + farenConvertedTemp + " degrees Celcius."
+        result.appendChild(p);
+        }
+    }
 
 convertBtn.addEventListener('click', convertTemp);
